99e6c240bdf8f575bcd81b612d74ea36a44dbce9
skyrllex <zeron00000hhs@gmail.com>
2026-01-21 20:33:31 +0700

    MOVE SECRET_KEY

--- apps/events/serializers.py
+++ apps/events/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from .models import Event, EventImage
 
-class EventImageSeirializer(serializers.ModelSerializer):
+class EventImageSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
 
 
     class Meta:
         model = EventImage
-        fields = ['id', 'image']
+        fields = ['id', 'image_url']
 
+    def get_image_url(self,obj):
+        if obj.image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
+        return None 
 
 
 class EventSerializer(serializers.ModelSerializer):
-    images = EventImageSeirializer(many=True, read_only=True)
+    images = EventImageSerializer(many=True, read_only=True)
     
     class Meta:
         model = Event
-        fields = ['name','start_date','end_date','images','status'] 
+        fields = ['id','name','start_date','end_date','images','status'] 
